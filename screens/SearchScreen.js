98c4fddd5b3b70daab73b4f965963e29e39e3da3
Abdul Muksith Rizvi <57699415+abdulmuksith3@users.noreply.github.com>
2021-12-28 21:54:50 +0300

    SearchScreen - MockData
    - Search screen completed with mock data

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,106 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, LogBox} from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions, TouchableOpacity, TextInput} from 'react-native';
+import {colors, font} from '../theme/theme'
+import { Icon } from 'react-native-elements';
 
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
-export default function SearchScreen() {
+export default function SearchScreen({navigation}) {
+  const [icon, setIcon] = useState(false);
+  const [sellers, setSellers] = useState([
+    {
+      status: true
+    },
+    {
+      status: true
+    },
+    {
+      status: true
+    },
+    {
+      status: false
+    },
+    {
+      status: true
+    },
+    {
+      status: true
+    },
+    {
+      status: true
+    },
+    {
+      status: false
+    }
+  ]);
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>SearchScreen</Text>
+    <View style={styles.container}>
+
+      <View style={styles.header}>
+        <TouchableOpacity onPress={()=> navigation.goBack()} style={styles.headerLeft}>
+          <Icon
+              size={25}
+              type="feather"
+              name={"arrow-left"}
+              color={colors.BLACK}
+          />
+        </TouchableOpacity>
+        <View style={styles.headerCenter}>
+          <Text style={styles.headerText}>Search</Text>
+        </View>
+        <View style={styles.headerRight}></View>
+      </View>
+
+
+      <View style={styles.center}>
+        <View style={styles.searchContainer}>
+          <TouchableOpacity onPress={()=>console.log("SEARCHING")} style={styles.searchButton}>
+            <Icon
+                size={25}
+                type={"feather"}
+                name={"search"}
+                color={colors.GRAY}
+            />
+          </TouchableOpacity>
+          <TextInput 
+            style={styles.searchInput}
+            // value="Jose"
+            onChangeText={(text)=>console.log(text)}
+            placeholder='Search for a seller'
+          />
+        </View>
+      </View>
+
+
+      <View style={styles.body}>
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.innerBody}>
+            {sellers && sellers.filter(x=> x.status === true).map((item, index) =>
+              <TouchableOpacity key={index} onPress={()=>navigation.navigate("SellerScreen")} style={[styles.sellerContainer, index%2 !== 0 && {marginLeft:"7%"}]}>
+                <View style={styles.sellerContainerTop}>
+                  {icon ?
+                    <Text style={styles.sellerUserImage}>IMG</Text>
+                    :
+                    <View style={styles.sellerUserIcon}>
+                      <Icon
+                          size={28}
+                          type="feather"
+                          name={"user"}
+                          color={colors.WHITE}
+                      />
+                    </View>
+                  }
+                </View>
+                <View style={styles.sellerContainerBottom}>
+                  <Text style={styles.sellerNameText}>Name</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          </View>                    
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -14,8 +108,125 @@ export default function SearchScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.WHITE,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingLeft:"1%",
+    paddingRight:"1%"
   },
+
+
+  //------------- HEADER ------------- 
+  header:{
+    flex:1,
+    // backgroundColor: "red",
+    flexDirection:"row"
+  },
+  headerLeft:{
+    flex:1,
+    // backgroundColor:"yellow",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  headerCenter:{
+    flex:5,
+    // backgroundColor:"yellow",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  headerText:{
+    fontSize: 14,
+    fontFamily: font.MEDIUM,
+    color: colors.BLACK
+  },
+  headerRight:{
+    flex:1,
+    // backgroundColor:"yellow"
+  },
+
+  
+  //------------- CENTER ------------- 
+  center:{
+    flex:1,
+    // backgroundColor: "blue",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  searchContainer:{
+    backgroundColor: colors.LIGHT_GRAY,
+    width:"90%",
+    flexDirection:"row",
+    height:45,
+    borderRadius:10,
+    alignItems:"center"
+  },
+  searchButton:{
+    // backgroundColor:"yellow",
+    width:"15%"
+  },
+  searchInput:{
+    // backgroundColor:"pink",
+    width:"85%",
+    fontFamily: font.MEDIUM,
+    fontSize: 14,
+    color: colors.GRAY
+  },
+
+  //------------- BODY ------------- 
+  body:{
+    flex:7,
+    // backgroundColor: "red"
+  },
+  scrollView:{
+    // backgroundColor:"yellow",
+  },
+  innerBody:{
+    // backgroundColor:"blue",
+    margin: "4.5%",
+    flexDirection:"row",
+    flexWrap:"wrap",
+    // justifyContent:"center"
+  },
+  sellerContainer:{
+    backgroundColor: colors.WHITE,
+    height: 190,
+    width: "45%",
+    borderRadius:10,
+    marginBottom:"8%",
+    shadowColor: colors.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  sellerContainerTop:{
+    height:"60%",
+    justifyContent:"flex-end",
+    alignItems:"center"
+  },
+  sellerUserIcon:{
+    width:80,
+    height:80,
+    borderRadius:160,
+    backgroundColor: colors.DARK_PURPLE,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  sellerUserImage:{
+
+  },
+  sellerContainerBottom:{
+    height:"40%",
+    justifyContent:"center",
+    alignSelf:"center"
+  },
+  sellerNameText:{
+    fontSize:14,
+    fontFamily:font.MEDIUM,
+    color: colors.BLACK
+  },
+
 });
